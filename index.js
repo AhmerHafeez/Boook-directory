dc5ed9f8214432b20ecc5edec622f2566b0d54cf
AhmerHafeez <ranaahmer4956270@gmail.com>
2023-12-01 13:39:00 +0500

    Resolved merge conflicts and applied local changes

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ const bookSchema = new mongoose.Schema({
   title: String,
   author: String,
   genre: String,
+  comments: [{
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    user: String,
+    text: String,
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    likedBy: [String],
+    dislikedBy: [String],
+  }],
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
 });
 
 const Book = mongoose.model("Book", bookSchema);
@@ -47,6 +58,15 @@ app.get("/books", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+// Get all books
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 // Search for books
 app.get("/books/search", async (req, res) => {
   const query = req.query.q.toLowerCase();
@@ -63,7 +83,6 @@ app.get("/books/search", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 // Get all authors
 app.get("/authors", async (req, res) => {
   try {
@@ -84,11 +103,125 @@ app.post("/AddBooks", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+//comment on a book
+app.post("/books/:id/comments", async (req, res) => {
+  const bookId = req.params.id;
+  const { user, text } = req.body;
+  try {
+    const book = await Book.findById(bookId);
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" })
+    }
+    //addd new comment
+    book.comments.push({ user, text });
+    const result = await book.save();
+    res.status(200).json({ message: "Comment added Successfully", book: result });
+  } catch (error) {
+    console.log("error adding comment", error);
+    res.status(500), json({ error: "Internal server error" })
+  }
+});
+//...like post
+app.post("/books/:bookId/comments/:commentsId/like", async (req, res) => {
+  const bookId = req.params.bookId;
+  const commentsId = req.params.commentsId;
+  const userName = req.body.userName;
+
+  try {
+    const book = await Book.findById(bookId);
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    const comment = book.comments.id(commentsId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    // Check if the user has already disliked the comment
+    if (comment.likedBy.includes(userName)) {
+      return res.status(400).json({ error: "You have already liked this comment" });
+    }
+
+    // Increment the dislikes counter and add the user to dislikedBy array
+    comment.likes += 1;
+    comment.likedBy.push(userName);
+
+    await book.save();
+
+    // Remove null values from dislikedBy array in the response
+    comment.likedBy = comment.likedBy.filter(Boolean);
+
+    res.json({
+      message: "Comment liked successfully",
+      book: {
+        ...book.toObject(),
+        comments: book.comments.map(c => ({
+          ...c.toObject(),
+          likes: c.likes,
+          likedBy: c.likedBy,
+        })),
+      },
+    });
+  } catch (error) {
+    console.error("Error liking comment:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+//.............diskike api
+app.post("/books/:bookId/comments/:commentId/dislike", async (req, res) => {
+  const bookId = req.params.bookId;
+  const commentId = req.params.commentId;
+  const userName = req.body.userName;
+
+  try {
+    const book = await Book.findById(bookId);
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    const comment = book.comments.id(commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    // Check if the user has already disliked the comment
+    if (comment.dislikedBy.includes(userName)) {
+      return res.status(400).json({ error: "You have already disliked this comment" });
+    }
+
+    // Increment the dislikes counter and add the user to dislikedBy array
+    comment.dislikes += 1;
+    comment.dislikedBy.push(userName);
+    await book.save();
+
+    // Remove null values from dislikedBy array in the response
+    comment.dislikedBy = comment.dislikedBy.filter(Boolean);
+
+    res.json({
+      message: "Comment disliked successfully",
+      book: {
+        ...book.toObject(),
+        comments: book.comments.map(c => ({
+          ...c.toObject(),
+          dislikes: c.dislikes,
+          dislikedBy: c.dislikedBy,
+        })),
+      },
+    });
+  } catch (error) {
+    console.error("Error disliking comment:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // Update a book by ID
 app.put("/books/:id", async (req, res) => {
   const bookId = req.params.id;
   const updatedBook = req.body;
-  
+
   try {
     const result = await Book.findByIdAndUpdate(bookId, updatedBook, { new: true });
 
@@ -112,7 +245,14 @@ app.delete("/books/:id", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
+app.delete("/api/data", async (req, res) => {
+  try {
+    const result = await Book.deleteMany({});
+    res.json({ message: "all data deleted" })
+  } catch (error) {
+    res.status(404).json({ error: "error deletng data" })
+  }
+})
 // echo "# Book-directory" >> README.md
 //   git init
 //   git add README.md
